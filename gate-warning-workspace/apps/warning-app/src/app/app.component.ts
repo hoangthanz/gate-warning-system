8ebde12a53f865dbee03e4dd919ba3e81c9a32b3
KV4-PM1-2359\xuanh <hoangnx.it4@vnpt.vn>
2022-05-12 10:13:22 +0700

    initialize app

---
+++ gate-warning-workspace/apps/warning-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gate-warning-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'warning-app';
+}
